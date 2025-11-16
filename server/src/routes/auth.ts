4e6dec0c8d4e8a66e423cd08ccde926ba17948e0
RohanMishra-179 <kingbooker124@gmail.com>
2025-11-16 13:25:45 +0530

    1. Added jsonwebtoken, bcryptjs, cookie-parser and their respective types
    2. Created a new 'User' table and established its relationships with 'Endpoint', and 'Billing' table
    
    3. Created a new 'auth.ts' file to handle user registration and login

---
+++ server/src/routes/auth.ts
@@ -0,0 +1,41 @@
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcryptjs";
+import { Router } from "express";
+import jwt from "jsonwebtoken";
+
+const router: ReturnType<typeof Router> = Router();
+const prisma = new PrismaClient();
+
+// User Registration
+router.post("/register", async (req, res) => {
+  const { email, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  try {
+    const user = await prisma.user.create({
+      data: { email, password: hashedPassword },
+    });
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(500).json({ error: "User registration failed" });
+  }
+});
+
+// User Login
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await prisma.user.findUnique({ where: { email } });
+
+  if (!user || !(await bcrypt.compare(password, user.password))) {
+    return res.status(401).json({ error: "Invalid credentials" });
+  }
+
+  const jwtSecret = process.env["JWT_SECRET"];
+
+  if (!jwtSecret) {
+    throw new Error("JWT_SECRET is not defined in environment variables");
+  }
+
+  const token = jwt.sign({ id: user.id }, jwtSecret, { expiresIn: "2h" });
+  return res.json({ token });
+});
